2fd95f9f69390379c3ff2d0602133280e5061f4d
Yuri Solyanik <jura.s.n@gmail.com>
2025-05-22 09:54:48 +0500

    Добавил решение

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -3,8 +3,39 @@
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    ui->radioButton->setObjectName("rb1");
+    ui->radioButton_2->setObjectName("rb2");
+
+    ui->comboBox->addItem("Item 1");
+    ui->comboBox->addItem("Item 2");
+    ui->comboBox->addItem("Item 3");
+    ui->comboBox->addItem("Item 4");
+    ui->comboBox->addItem("Item 5");
+
+    ui->pushButton->setText("Do it");
+
+    ui->pushButton->setCheckable(true);
+
+    ui->progressBar->setRange(0, 10);
+    ui->progressBar->setValue(0);
+
+    connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::onPushButtonClicked);
 }
 
 MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::onPushButtonClicked() {
+    int currentValue = ui->progressBar->value();
+    int max = ui->progressBar->maximum();
+    int step = max * 0.10;  // 10% от максимума
+    int newValue = currentValue + step;
+
+    if (newValue > max) {
+        newValue = 0;
+    }
+
+    ui->progressBar->setValue(newValue);
 }
--- MainWindow.h
+++ MainWindow.h
@@ -15,6 +15,9 @@ class MainWindow : public QMainWindow {
     MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+  private slots:
+    void onPushButtonClicked();
+
   private:
     Ui::MainWindow* ui;
 };
